3bb66a58ef49aac8f1699dca8b02a90d9af0329d
Kafi Anan Rafi <kafianan01@gmail.com>
2025-01-27 15:30:15 +0600

    Fix Navbar UI issue

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,16 @@
+import Link from "next/link";
 import { AiOutlineDingtalk } from "react-icons/ai";
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 w-full bg-slate-950 py-4 border-b px-2">
+    <div className="fixed top-0 w-full bg-slate-950 py-4 border-b px-2 z-50">
       <div className="container mx-auto">
         <div className="flex items-center justify-between">
           <div className="text-blue-200 font-bold text-lg cursor-pointer flex items-center justify-center gap-1 hover:text-blue-100 transition duration-500">
-            <AiOutlineDingtalk size={25} />
-            NeuroTalk
+            <Link href={"/"} className="flex gap">
+              <AiOutlineDingtalk size={25} />
+              NeuroTalk
+            </Link>
           </div>
         </div>
       </div>
